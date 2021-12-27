251935daa4b8ad4d53267a72ce2e878d58b18d9e
Ivan-Grigorev <iv.st.grigorev@gmail.com>
2021-12-27 11:07:54 +0200

    Module 12

--- Module_12/views.py
+++ Module_12/views.py
@@ -9,7 +9,7 @@ from init_db import db
 async def index(request):
     cursor_ow = db.open_weather.find().sort([['_id', -1]]).limit(1)
     for document_ow in await cursor_ow.to_list(length=None):
-        return aiohttp_jinja2.render_template('show_weather.html', request, {
+        response = aiohttp_jinja2.render_template('show_weather.html', request, {
             'ow_name': document_ow['name'],
             'ow_temp_c': (document_ow['main']['temp'] - 273.15).__round__(1),
             'ow_temp_f': ((document_ow['main']['temp'] - 273.15) * 9 / 5 + 32).__round__(1),
@@ -22,10 +22,11 @@ async def index(request):
             'ow_humidity': document_ow['main']['humidity'],
             'ow_dt': datetime.fromtimestamp(document_ow['dt'])
         })
+        return response
 
     cursor_wi = db.weather_api.find().sort([['_id', -1]]).limit(1)
     for document_wi in await cursor_wi.to_list(length=None):
-        return aiohttp_jinja2.render_template('show_weather.html', request, {
+        response = aiohttp_jinja2.render_template('show_weather.html', request, {
             'wi_name': document_wi['location']['name'],
             'wi_temp_c': document_wi['current']['temp_c'],
             'wi_temp_f': document_wi['current']['temp_f'],
@@ -38,3 +39,4 @@ async def index(request):
             'wi_humidity': document_wi['current']['humidity'],
             'wi_dt': datetime.fromtimestamp(document_wi['current']['last_updated_epoch'])
         })
+        return response
