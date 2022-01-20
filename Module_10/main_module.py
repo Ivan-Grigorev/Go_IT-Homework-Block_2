d6af396c77d2dedda39cb726149463c054dcbeae
Ivan-Grigorev <iv.st.grigorev@gmail.com>
2022-01-20 13:31:14 +0200

    Module 10

--- Module_10/main_module.py
+++ Module_10/main_module.py
@@ -5,7 +5,7 @@ from pymongo import MongoClient
 
 
 client = MongoClient(
-        'mongodb+srv://Ivan_Grigorev:mongo_cloud_28.09_ig@cluster0.nzrg5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+        'mongodb+srv://**********:**********@cluster0.nzrg5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
     )
 
 db = client.module_10_hw
