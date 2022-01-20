e9e1a2261167ac773dff31ef554241e7aa3a67f6
Ivan-Grigorev <iv.st.grigorev@gmail.com>
2022-01-20 11:56:10 +0200

    Module 11

--- Module_11/app_folder/__init__.py
+++ Module_11/app_folder/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:pstgr_28.09_ig@localhost:5432/module_11.hw'
+app.config['SQLALCHEMY_DATABASE_URI'] = '**********://**********:**********@**********:**********/**********'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
